5b99cec9c58e2d241ea9a3be6d7727e9fb1d5ed1
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-10-20 10:11:11 +0000

    create table if it does not exist

--- backend/src/lib/db.ts
+++ backend/src/lib/db.ts
@@ -1,3 +1,8 @@
 import { Database } from 'bun:sqlite';
 
 export const db = new Database('../db/directories.sqlite');
+
+db.query(`CREATE TABLE IF NOT EXISTS directories (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL UNIQUE
+)`);
