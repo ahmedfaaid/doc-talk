fa011b252848512dace9366cbc55307cfa6696e7
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-10-20 11:56:06 +0000

    setup cors, dotenv and api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,18 @@
+import 'dotenv/config';
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+import { indexDirectory, query } from './controllers/directory';
 
 const app = new Hono();
 
-app.get('/', c => {
-  return c.text('Hello Hono!');
+app.use('/*', cors());
+
+app.get('/status', c => {
+  return c.json({ message: 'ok' }, 200);
 });
+
+app.post('/index-directory', indexDirectory);
+app.get('/query', query);
 
 export default {
   port: 5155,
