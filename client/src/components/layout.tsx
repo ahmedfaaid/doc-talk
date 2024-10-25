7c86dfe6cabd0b3dc7606209042d52aef615a77a
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-10-25 07:45:46 +0000

    move directory dialog provider to main.tsx

--- client/src/components/layout.tsx
+++ client/src/components/layout.tsx
@@ -1,15 +1,14 @@
 import { ReactNode } from 'react';
-import SelectedDirectoryProvider from '../context/directory-dialog';
 import AppSidebar from './app-sidebar';
 import { SidebarProvider } from './ui/sidebar';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <SelectedDirectoryProvider>
+    <>
       <SidebarProvider>
         <AppSidebar />
         <main>{children}</main>
       </SidebarProvider>
-    </SelectedDirectoryProvider>
+    </>
   );
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './App.css';
+import SelectedDirectoryProvider from './context/directory-dialog';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <SelectedDirectoryProvider>
+      <App />
+    </SelectedDirectoryProvider>
   </React.StrictMode>
 );
