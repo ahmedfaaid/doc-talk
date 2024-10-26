93e34451630299569a6e0dc017d8631da6cbf37a
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-10-26 13:24:10 +0000

    add chatview to main app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import ChatView from './components/chat-view';
 import Layout from './components/layout';
 import NoDirectory from './components/no-directory';
 import { SelectedDirectoryContext } from './context/directory-dialog';
@@ -6,17 +7,7 @@ import { SelectedDirectoryContext } from './context/directory-dialog';
 function App() {
   const { directory } = useContext(SelectedDirectoryContext);
 
-  return (
-    <Layout>
-      {directory ? (
-        <div>
-          <p>Chat view</p>
-        </div>
-      ) : (
-        <NoDirectory />
-      )}
-    </Layout>
-  );
+  return <Layout>{directory ? <ChatView /> : <NoDirectory />}</Layout>;
 }
 
 export default App;
