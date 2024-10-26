732e3440c356fcb9900746abb6871de282cb346d
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-10-26 12:12:41 +0000

    height of the main content

--- client/src/components/layout.tsx
+++ client/src/components/layout.tsx
@@ -4,11 +4,11 @@ import { SidebarProvider } from './ui/sidebar';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <>
+    <div>
       <SidebarProvider>
         <AppSidebar />
-        <main className='w-full min-h-full'>{children}</main>
+        <main className='w-full h-screen'>{children}</main>
       </SidebarProvider>
-    </>
+    </div>
   );
 }
