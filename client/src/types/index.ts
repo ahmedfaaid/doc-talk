bd042cb253fdf82e78fd9c6838b70482627fdb86
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-11-23 14:56:59 +0000

    client: Directory and ApiResponse types

---
+++ client/src/types/index.ts
@@ -0,0 +1,12 @@
+export interface Directory {
+  id: number;
+  name: string;
+  vector_path: string;
+  indexed: boolean;
+}
+
+export interface ApiResponse {
+  message: string;
+  code: number;
+  directories: Directory[] | null;
+}
