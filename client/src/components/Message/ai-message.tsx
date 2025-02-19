bcc3eb8678a17b1c43e4098ac389ef1759d63091
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-02-19 23:05:36 +0000

    client: render ai markdown response

--- client/src/components/Message/ai-message.tsx
+++ client/src/components/Message/ai-message.tsx
@@ -1,4 +1,5 @@
 import { Message } from '@/types';
+import Markdown from 'react-markdown';
 
 interface AiMessageProps {
   message: Message;
@@ -8,7 +9,7 @@ export default function AiMessage({ message }: AiMessageProps) {
   return (
     <div className='receive-chat relative flex justify-start'>
       <div className='mb-2 max-w-[80%] rounded bg-zinc-200 px-5 py-2 text-sm font-light text-black'>
-        <p>{message.content}</p>
+        <Markdown>{message.content}</Markdown>
       </div>
     </div>
   );
