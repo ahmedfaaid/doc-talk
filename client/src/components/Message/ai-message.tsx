297db56992b058228398887827b128ecf2d36172
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-03-03 17:21:51 +0000

    client: use markdown renderer in ai message component

--- client/src/components/Message/ai-message.tsx
+++ client/src/components/Message/ai-message.tsx
@@ -1,5 +1,5 @@
 import { Message } from '@/types';
-import Markdown from 'react-markdown';
+import MarkdownRenderer from '../markdown-renderer';
 
 interface AiMessageProps {
   message: Message;
@@ -9,7 +9,7 @@ export default function AiMessage({ message }: AiMessageProps) {
   return (
     <div className='receive-chat relative flex justify-start'>
       <div className='mb-2 max-w-[80%] rounded bg-zinc-200 px-5 py-2 text-sm font-light text-black'>
-        <Markdown>{message.content}</Markdown>
+        <MarkdownRenderer content={message.content} />
       </div>
     </div>
   );
