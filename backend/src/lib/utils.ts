8698467180287abf96c76c1b0aa0e4855a222d4a
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-06-09 20:44:52 +0000

    backend: create vector store path utility function

--- backend/src/lib/utils.ts
+++ backend/src/lib/utils.ts
@@ -1,3 +1,7 @@
 export const getLastPathSegment = (path: string): string => {
   return path.substring(path.lastIndexOf('/') + 1);
 };
+
+export const createVectorStorePath = (name: string) => {
+  return `vectorstore/${name}`;
+};
