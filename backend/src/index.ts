2add9b3b39abc66f783e9f08a7f37a7fd8bf2e00
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-06-09 16:20:26 +0000

    backend: add get one thread to routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from 'hono';
 import { cors } from 'hono/cors';
 import { logger } from 'hono/logger';
 import { chat, indexDirectory, retrieveIndexedDirectory } from './controllers';
-import { createThread, getThreads } from './controllers/thread';
+import { createThread, getOneThread, getThreads } from './controllers/thread';
 
 const app = new Hono();
 
@@ -19,6 +19,7 @@ app.post('/chat', chat);
 app.get('/retrieve-directory', retrieveIndexedDirectory);
 app.post('/threads', createThread);
 app.get('/threads', getThreads);
+app.get('/threads/:id', getOneThread);
 
 export default {
   port: 5155,
