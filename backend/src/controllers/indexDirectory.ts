7ce875005430e806a027d5b1350901b40cac1f50
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-06-09 20:50:02 +0000

    backend: use new vectore path, embeddings and db operation functions

--- backend/src/controllers/indexDirectory.ts
+++ backend/src/controllers/indexDirectory.ts
@@ -3,21 +3,22 @@ import { DocxLoader } from '@langchain/community/document_loaders/fs/docx';
 import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
 import { PPTXLoader } from '@langchain/community/document_loaders/fs/pptx';
 import { HNSWLib } from '@langchain/community/vectorstores/hnswlib';
-import { OpenAIEmbeddings } from '@langchain/openai';
 import { Context } from 'hono';
 import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
 import { TextLoader } from 'langchain/document_loaders/fs/text';
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 import { access } from 'node:fs/promises';
-import { db } from '../../db';
+import { addDirectory } from '../../db/directory';
+import { embeddings } from '../lib/AI';
+import { createVectorStorePath } from '../lib/utils';
 
 export const indexDirectory = async (c: Context) => {
-  const { directoryPath, name } = await c.req.json();
-
   try {
+    const { directoryPath, name } = await c.req.json();
+
     await access(directoryPath!);
 
-    const vectorStoreBasePath: string = 'vectorstore';
+    const vector_path = createVectorStorePath(name);
 
     const loader = new DirectoryLoader(directoryPath!, {
       '.txt': path => new TextLoader(path),
@@ -46,41 +47,10 @@ export const indexDirectory = async (c: Context) => {
     });
     const splitText = await textSplitter.splitDocuments(docs);
 
-    const embeddings = new OpenAIEmbeddings({
-      apiKey: 'lm-studio',
-      model: 'nomic-ai/nomic-embed-text-v1.5-GGUF',
-      configuration: {
-        baseURL: 'http://localhost:1234/v1'
-      }
-    });
     const vectorStore = await HNSWLib.fromDocuments(splitText, embeddings);
-    await vectorStore.save(`${vectorStoreBasePath}/${name}`);
+    await vectorStore.save(vector_path);
 
-    const addDirectory = db.prepare(`
-      INSERT INTO directories (name, directory_path, vector_path, indexed)
-      SELECT $name, $directory_path, $vector_path, $indexed
-      WHERE NOT EXISTS (
-        SELECT 1
-        FROM directories
-        WHERE name = $name AND directory_path = $directory_path
-      );
-    `);
-    const getDirectory = db.prepare(`
-      SELECT * FROM directories 
-      WHERE name = $name AND directory_path = $directory_path;
-    `);
-
-    addDirectory.run({
-      $name: name,
-      $vector_path: `${vectorStoreBasePath}/${name}`,
-      $indexed: 1,
-      $directory_path: directoryPath
-    });
-
-    const directory = getDirectory.get({
-      $name: name,
-      $directory_path: directoryPath
-    });
+    const directory = addDirectory(directoryPath, name, vector_path, true);
 
     return c.json(
       { message: 'Directory indexed successfully', code: 201, directory },
