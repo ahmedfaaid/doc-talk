ed12d8ed697cf22aff3eb8b3078c4193b1571278
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-06-13 19:45:33 +0000

    backend: add thread controllers and update user controllers to general export

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -1,4 +1,20 @@
 import { chat } from './chat';
 import { indexDirectory, retrieveIndexedDirectory } from './directory';
+import {
+  createThread,
+  deleteOneThread,
+  getOneThread,
+  getThreads
+} from './thread';
+import { updateUser } from './user';
 
-export { chat, indexDirectory, retrieveIndexedDirectory };
+export {
+  chat,
+  createThread,
+  deleteOneThread,
+  getOneThread,
+  getThreads,
+  indexDirectory,
+  retrieveIndexedDirectory,
+  updateUser
+};
