3216568106af140eec7360f46cd4eaa93332dfce
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-06-13 19:19:10 +0000

    backend: add user schema to db client

--- backend/db/index.ts
+++ backend/db/index.ts
@@ -3,9 +3,10 @@ import { drizzle } from 'drizzle-orm/bun-sqlite';
 import * as directory from './schema/directory';
 import * as message from './schema/message';
 import * as thread from './schema/thread';
+import * as user from './schema/user';
 
 const sqlite = new Database(process.env.DB_FILE_NAME!);
 export const db = drizzle({
   client: sqlite,
-  schema: { ...directory, ...message, ...thread }
+  schema: { ...directory, ...message, ...thread, ...user }
 });
