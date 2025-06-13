647b1e975c3f50561443afa0e8c038fb1df17cf9
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-06-13 19:19:54 +0000

    backend: user db operations

---
+++ backend/db/operations/user.ts
@@ -0,0 +1,58 @@
+import { randomUUID } from 'crypto';
+import { asc, eq } from 'drizzle-orm';
+import { db } from '..';
+import { User } from '../../types';
+import { users } from '../schema/user';
+
+export const createUser = async (user: Omit<User, 'id'>): Promise<User> => {
+  const u: User = {
+    id: randomUUID(),
+    ...user
+  };
+
+  await db.insert(users).values(u);
+
+  return u;
+};
+
+export const getUser = async (
+  userId?: string,
+  email?: string
+): Promise<User | null> => {
+  const user = await db.query.users.findFirst({
+    where: userId ? eq(users.id, userId) : eq(users.email, email!)
+  });
+
+  return user as User | null;
+};
+
+export const getAllUsers = async (
+  limit: number = 50,
+  offset: number = 0
+): Promise<User[]> => {
+  const allUsers = await db.query.users.findMany({
+    orderBy: [asc(users.created_at)],
+    limit,
+    offset
+  });
+
+  return allUsers as User[];
+};
+
+export const updateUser = async (
+  userId: string,
+  user: Partial<
+    Pick<User, 'email' | 'first_name' | 'last_name' | 'is_company' | 'role'>
+  >
+): Promise<User> => {
+  const [update] = await db
+    .update(users)
+    .set({
+      ...user,
+      updated_at: new Date().toISOString()
+    })
+    .where(eq(users.id, userId))
+    .returning();
+
+  return update;
+};
