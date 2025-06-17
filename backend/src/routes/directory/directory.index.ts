2189d6ee29848aad6517513abfb003c093452ad1
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-06-17 16:40:57 +0000

    backend: directory router and add index directory to router

---
+++ backend/src/routes/directory/directory.index.ts
@@ -0,0 +1,10 @@
+import { directoryController } from '../../controllers';
+import { createRouter } from '../../lib/create-app';
+import * as routes from './directory.route';
+
+const router = createRouter().openapi(
+  routes.indexDirectory,
+  directoryController.indexDirectory
+);
+
+export default router;
