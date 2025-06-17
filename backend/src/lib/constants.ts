8e45a66371b8b712f4f306846de6398a239f3c01
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-06-17 13:21:41 +0000

    backend: zod error codes constants

--- backend/src/lib/constants.ts
+++ backend/src/lib/constants.ts
@@ -4,3 +4,15 @@ import * as HttpStatusPhrases from './http-status-phrases';
 export const notFoundSchema = createMessageObjectSchema(
   HttpStatusPhrases.NOT_FOUND
 );
+
+export const ZOD_ERROR_MESSAGES = {
+  REQUIRED: 'Required',
+  EXPECTED_NUMBER: 'Expected number, received nan',
+  NO_UPDATES: 'No updates provided',
+  INVALID_UPDATES: 'Invalid updates',
+  INVALID_JSON: 'Invalid JSON'
+};
+
+export const ZOD_ERROR_CODES = {
+  INVALID_UPDATES: 'invalid_updates'
+};
