3b85f27247624b9e954d8a72b0923faadc1ac6cf
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-06-17 14:16:48 +0000

    backend: modify update user controller to match open api route

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,16 +1,51 @@
 import { Context } from 'hono';
 import { updateUser as updtUser } from '../db/operations/user.operation';
+import { ZOD_ERROR_CODES, ZOD_ERROR_MESSAGES } from '../lib/constants';
+import { NOT_FOUND, OK, UNPROCESSABLE_ENTITY } from '../lib/http-status-codes';
+import { NOT_FOUND as NOT_FOUND_PHRASE } from '../lib/http-status-phrases';
+import { UpdateUserRoute } from '../routes/user/user.route';
+import { AppRouteHandler } from '../types';
 
-export const updateUser = async (c: Context) => {
+export const updateUser: AppRouteHandler<UpdateUserRoute> = async (
+  c: Context
+) => {
   try {
     const userId = c.req.param('id');
     const { user } = await c.req.json();
 
+    if (Object.keys(user).length === 0) {
+      return c.json(
+        {
+          success: false,
+          error: {
+            issues: [
+              {
+                code: ZOD_ERROR_CODES.INVALID_UPDATES,
+                path: [],
+                message: ZOD_ERROR_MESSAGES.NO_UPDATES
+              }
+            ],
+            name: 'ZodError'
+          }
+        },
+        UNPROCESSABLE_ENTITY
+      );
+    }
+
     const updatedUser = await updtUser(userId, user);
+
+    if (!updtUser)
+      return c.json(
+        {
+          message: NOT_FOUND_PHRASE
+        },
+        NOT_FOUND
+      );
+
     const { password, ...rest } = updatedUser;
 
-    return c.json(rest, 200);
+    return c.json(rest, OK);
   } catch (error) {
-    return c.json({ error: 'Failed to update user' }, 500);
+    return c.json({ message: 'Failed to update user' }, 500);
   }
 };
