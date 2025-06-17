748f4ad0c129b3343d60aad50f30f7aef8033718
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-06-17 21:16:16 +0000

    backend: add get threads route and controller to router

--- backend/src/routes/thread/thread.index.ts
+++ backend/src/routes/thread/thread.index.ts
@@ -2,9 +2,8 @@ import { threadController } from '../../controllers';
 import { createRouter } from '../../lib/create-app';
 import * as routes from './thread.route';
 
-const router = createRouter().openapi(
-  routes.createThread,
-  threadController.createThread
-);
+const router = createRouter()
+  .openapi(routes.createThread, threadController.createThread)
+  .openapi(routes.getThreads, threadController.getThreads);
 
 export default router;
