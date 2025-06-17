046ef77d90f3a1e5f912b878dd1600fb4ff10df4
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-06-17 12:40:35 +0000

    backend: update user route

---
+++ backend/src/routes/user.route.ts
@@ -0,0 +1,38 @@
+import { createRoute } from '@hono/zod-openapi';
+import { selectUserSchema, updateUserSchema } from '../db/schema/user.schema';
+import { notFoundSchema } from '../lib/constants';
+import createErrorSchema from '../lib/create-error-schema';
+import * as HttpStatusCodes from '../lib/http-status-codes';
+import { jsonContent, jsonContentRequired } from '../lib/json-content';
+import userIdParamsSchema from '../lib/user-id-params';
+
+const tags = ['users'];
+
+export const updateUser = createRoute({
+  tags,
+  method: 'put',
+  path: '/users/{id}',
+  request: {
+    params: userIdParamsSchema,
+    body: jsonContentRequired(updateUserSchema, 'The user data to update')
+  },
+  responses: {
+    [HttpStatusCodes.OK]: {
+      content: {
+        'application/json': {
+          schema: selectUserSchema
+        }
+      },
+      description: 'The updated user'
+    },
+    [HttpStatusCodes.UNPROCESSABLE_ENTITY]: jsonContent(
+      createErrorSchema(updateUserSchema).or(
+        createErrorSchema(userIdParamsSchema)
+      ),
+      'Validation error for the user data or user ID'
+    ),
+    [HttpStatusCodes.NOT_FOUND]: jsonContent(notFoundSchema, 'User not found')
+  }
+});
+
+export type UpdateUserRoute = typeof updateUser;
