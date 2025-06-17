0ddbe34930dfa72e02b8b27fb6dde29c6fd975f2
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-06-17 14:49:00 +0000

    backend: fix output & schema paths, use env config for db file name

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'drizzle-kit';
+import env from './src/lib/env';
 
 export default defineConfig({
-  out: './db/migrations',
-  schema: './db/schema',
+  out: './src/db/migrations',
+  schema: './src/db/schema',
   dialect: 'sqlite',
   dbCredentials: {
-    url: process.env.DB_FILE_NAME!
+    url: env.DB_FILE_NAME
   }
 });
