04828bdf8b084827db2204780e858f03666cc024
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-06-17 12:56:37 +0000

    backend: env setup

---
+++ backend/src/lib/env.ts
@@ -0,0 +1,38 @@
+import { z, ZodError } from 'zod';
+
+const EnvSchema = z
+  .object({
+    HUGGING_FACE_TOKEN: z.string().optional(),
+    DB_FILE_NAME: z.string().url(),
+    JWT_SECRET_KEY: z.string(),
+    PORT: z.coerce.number().default(5155),
+    NODE_ENV: z
+      .enum(['development', 'production', 'test'])
+      .default('development')
+  })
+  .superRefine((input, ctx) => {
+    if (!input.JWT_SECRET_KEY) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.invalid_type,
+        expected: 'string',
+        received: 'undefined',
+        path: ['JWT_SECRET_KEY'],
+        message: 'JWT secret key is required.'
+      });
+    }
+  });
+
+export type env = z.infer<typeof EnvSchema>;
+
+let env: env;
+
+try {
+  env = EnvSchema.parse(process.env);
+} catch (error) {
+  const err = error as ZodError;
+  console.error('🔴 Error with env:');
+  console.error(err.flatten().fieldErrors);
+  process.exit(1);
+}
+
+export default env;
