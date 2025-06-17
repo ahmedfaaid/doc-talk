abe6af1e5a0fda4fe6f82b077a1fec8007f9c5d1
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-06-17 21:49:57 +0000

    backend: add delete one thread route and controller to router

--- backend/src/routes/thread/thread.index.ts
+++ backend/src/routes/thread/thread.index.ts
@@ -5,6 +5,7 @@ import * as routes from './thread.route';
 const router = createRouter()
   .openapi(routes.createThread, threadController.createThread)
   .openapi(routes.getThreads, threadController.getThreads)
-  .openapi(routes.getOneThread, threadController.getOneThread);
+  .openapi(routes.getOneThread, threadController.getOneThread)
+  .openapi(routes.deleteOneThread, threadController.deleteOneThread);
 
 export default router;
