850f5cc404e496676203c9960411a8ba6f62be6b
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-06-17 14:29:07 +0000

    backend: create app

---
+++ backend/src/lib/create-app.ts
@@ -0,0 +1,35 @@
+import { OpenAPIHono } from '@hono/zod-openapi';
+import notFound from '../middlewares/not-found';
+import onError from '../middlewares/on-error';
+import pinoLogger from '../middlewares/pino-logger';
+import { AppBindings } from '../types';
+import defaultHook from './default-hook';
+
+export function createRouter() {
+  return new OpenAPIHono<AppBindings>({
+    strict: false,
+    defaultHook
+  });
+}
+
+export default function createApp() {
+  const app = createRouter();
+
+  app.notFound(notFound);
+  app.onError(onError);
+  app.use(pinoLogger());
+
+  app.get('/error', c => {
+    c.status(422);
+    c.var.logger.info('Some logs here');
+    throw new Error('Something went wrong');
+  });
+
+  app.get('/', c => {
+    return c.json({
+      message: 'Hello from Doc-Talk'
+    });
+  });
+
+  return app;
+}
