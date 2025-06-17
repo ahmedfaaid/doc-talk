b363e1a4780109cd50efd17d9f97ac224ed63343
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-06-17 15:15:50 +0000

    backend: mount open api config on app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,12 @@
 import createApp from './lib/create-app';
+import openApiConfig from './lib/open-api-config';
 import user from './routes/user/user.index';
 
 const app = createApp();
 
 const routes = [user];
+
+openApiConfig(app);
 
 routes.forEach(route => {
   app.route('/', route);
