64d5c10226667c8698887fa9718a3aef8803833c
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-06-17 16:41:23 +0000

    backend: mount directory routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,11 @@
 import createApp from './lib/create-app';
 import openApiConfig from './lib/open-api-config';
+import directory from './routes/directory/directory.index';
 import user from './routes/user/user.index';
 
 const app = createApp();
 
-const routes = [user];
+const routes = [user, directory];
 
 openApiConfig(app);
 
