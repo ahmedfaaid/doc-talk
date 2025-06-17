e0452d0a128f84ea63bff7b7fcbadfbf452c82c9
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-06-17 17:15:38 +0000

    backend: add retrieve indexed directory route and controller to router

--- backend/src/routes/directory/directory.index.ts
+++ backend/src/routes/directory/directory.index.ts
@@ -2,9 +2,11 @@ import { directoryController } from '../../controllers';
 import { createRouter } from '../../lib/create-app';
 import * as routes from './directory.route';
 
-const router = createRouter().openapi(
-  routes.indexDirectory,
-  directoryController.indexDirectory
-);
+const router = createRouter()
+  .openapi(routes.indexDirectory, directoryController.indexDirectory)
+  .openapi(
+    routes.retrieveIndexedDirectory,
+    directoryController.retrieveIndexedDirectory
+  );
 
 export default router;
