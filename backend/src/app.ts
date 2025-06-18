230f91db69f52d5914d3b05d49308872f33dc096
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-06-18 21:34:03 +0000

    backend: add chat router to app routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,13 @@
 import createApp from './lib/create-app';
 import openApiConfig from './lib/open-api-config';
+import chat from './routes/chat/chat.index';
 import directory from './routes/directory/directory.index';
 import thread from './routes/thread/thread.index';
 import user from './routes/user/user.index';
 
 const app = createApp();
 
-const routes = [user, directory, thread];
+const routes = [user, directory, thread, chat];
 
 openApiConfig(app);
 
