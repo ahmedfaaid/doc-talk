f4246967c4ab848b9211933e98793156e5823f36
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-06-19 09:41:35 +0000

    backend: add auth to app routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import createApp from './lib/create-app';
 import openApiConfig from './lib/open-api-config';
+import auth from './routes/auth/auth.index';
 import chat from './routes/chat/chat.index';
 import directory from './routes/directory/directory.index';
 import thread from './routes/thread/thread.index';
@@ -7,7 +8,7 @@ import user from './routes/user/user.index';
 
 const app = createApp();
 
-const routes = [user, directory, thread, chat];
+const routes = [user, directory, thread, chat, auth];
 
 openApiConfig(app);
 
