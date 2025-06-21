de6b6adaf9e5c6c7c929f06ffc1e57d7c88303b8
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-06-21 18:11:28 +0000

    backend: register route

--- backend/src/routes/auth/auth.route.ts
+++ backend/src/routes/auth/auth.route.ts
@@ -1,5 +1,9 @@
 import { createRoute, z } from '@hono/zod-openapi';
-import { authSchema, loginSchema } from '../../db/schema/user.schema';
+import {
+  authSchema,
+  loginSchema,
+  registerSchema
+} from '../../db/schema/user.schema';
 import { serverErrorSchema } from '../../lib/constants';
 import * as HttpStatusCodes from '../../lib/http-status-codes';
 import { jsonContent, jsonContentRequired } from '../../lib/json-content';
@@ -28,4 +32,33 @@ export const login = createRoute({
   }
 });
 
+export const register = createRoute({
+  tags,
+  method: 'post',
+  path: '/auth/register',
+  request: {
+    body: jsonContentRequired(
+      z.object({
+        user: registerSchema
+      }),
+      'Register request body'
+    )
+  },
+  responses: {
+    [HttpStatusCodes.CREATED]: jsonContent(authSchema, 'Register response'),
+    [HttpStatusCodes.CONFLICT]: jsonContent(
+      z.object({
+        message: z.string(),
+        code: z.number()
+      }),
+      'Conflict response'
+    ),
+    [HttpStatusCodes.INTERNAL_SERVER_ERROR]: jsonContent(
+      serverErrorSchema,
+      'Server error'
+    )
+  }
+});
+
 export type LoginRoute = typeof login;
+export type RegisterRoute = typeof register;
