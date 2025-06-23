0e87b2b171a0f16b7b635712eb63bfb585039cf1
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-06-23 16:02:00 +0000

    backend: directory owner id and one to many relation to user

--- backend/src/db/schema/directory.schema.ts
+++ backend/src/db/schema/directory.schema.ts
@@ -1,6 +1,8 @@
 import { randomUUID } from 'crypto';
+import { relations } from 'drizzle-orm';
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 import { z } from 'zod';
+import { users } from './user.schema';
 
 export const directories = sqliteTable('directories', {
   id: text('id', { mode: 'text' }).primaryKey().default(randomUUID()).notNull(),
@@ -8,8 +10,20 @@ export const directories = sqliteTable('directories', {
   directory_path: text('directory_path', { mode: 'text' }).notNull().unique(),
   vector_path: text('vector_path', { mode: 'text' }).notNull().unique(),
   indexed: integer('indexed', { mode: 'boolean' }).notNull().default(false),
+  owner_id: text('owner_id', { mode: 'text' }).notNull(),
+  access_level: text('access_level', {
+    mode: 'text',
+    enum: ['user', 'admin', 'superadmin']
+  }),
   created_at: text('created_at').notNull().default(new Date().toISOString())
 });
+
+export const directoryRelations = relations(directories, ({ one }) => ({
+  owner: one(users, {
+    fields: [directories.owner_id],
+    references: [users.id]
+  })
+}));
 
 export const insertDirectorySchema = z.object({
   name: z.string(),
