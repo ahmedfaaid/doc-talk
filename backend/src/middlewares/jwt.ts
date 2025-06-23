2cbf91f8b16e0886bbf9f5a83c088f3d9b89a794
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-06-23 09:37:49 +0000

    backend: jwt middleware

---
+++ backend/src/middlewares/jwt.ts
@@ -0,0 +1,36 @@
+import { Context, Next } from 'hono';
+import { verify } from 'hono/jwt';
+import env from '../lib/env';
+import { INTERNAL_SERVER_ERROR, UNAUTHORIZED } from '../lib/http-status-codes';
+import { JWTPayload } from '../types';
+
+const jwtMiddleware = async (c: Context, next: Next) => {
+  try {
+    const authHeader = c.req.header('Authorization');
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return c.json(
+        { message: 'Unauthorized', code: UNAUTHORIZED },
+        UNAUTHORIZED
+      );
+    }
+
+    const token = authHeader.split(' ')[1];
+    const payload = (await verify(token, env.JWT_SECRET_KEY)) as JWTPayload;
+
+    if (payload.exp < Math.floor(Date.now() / 1000)) {
+      return c.json(
+        { message: 'Unauthorized', code: UNAUTHORIZED },
+        UNAUTHORIZED
+      );
+    }
+
+    c.set('user', { id: payload.id, email: payload.email });
+
+    await next();
+  } catch (error) {
+    return c.json({ message: (error as Error).message }, INTERNAL_SERVER_ERROR);
+  }
+};
+
+export default jwtMiddleware;
