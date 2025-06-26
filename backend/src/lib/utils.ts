9bd9762ee7e90280d076e93679844f1c49424c91
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-06-26 12:09:34 +0000

    backend: create upload file path function

--- backend/src/lib/utils.ts
+++ backend/src/lib/utils.ts
@@ -1,7 +1,13 @@
+import { UPLOAD_DIR } from './constants';
+
 export const getLastPathSegment = (path: string): string => {
   return path.substring(path.lastIndexOf('/') + 1);
 };
 
 export const createVectorStorePath = (name: string) => {
   return `vectorstore/${name}`;
+};
+
+export const createUploadFilePath = (name: string, userId: string) => {
+  return `${UPLOAD_DIR}/${userId}-${name}`;
 };
