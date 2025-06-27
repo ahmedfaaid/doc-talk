f1ef0f4c60d27f38c77640acfd6325860a74a584
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-06-27 13:36:02 +0000

    backend: export file controller

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -1,5 +1,6 @@
 export * as authController from './auth.controller';
 export * as chatController from './chat.controller';
 export * as directoryController from './directory.controller';
+export * as fileController from './file.controller';
 export * as threadController from './thread.controller';
 export * as userController from './user.controller';
