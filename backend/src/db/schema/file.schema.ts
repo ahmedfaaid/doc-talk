8a632b1edd0cda84dc28acd2578f090eeecc9d51
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-06-29 18:57:25 +0000

    backend: add vector store path, create upload status, vector status, upload and vector completed at columns

--- backend/src/db/schema/file.schema.ts
+++ backend/src/db/schema/file.schema.ts
@@ -14,6 +14,7 @@ export const files = sqliteTable('files', {
   }).notNull(),
   original_path: text('original_path', { mode: 'text' }).notNull(),
   upload_path: text('upload_path', { mode: 'text' }).notNull(),
+  vector_store_path: text('vector_store_path', { mode: 'text' }),
   size: text('size', { mode: 'text' }).notNull(),
   batch_id: text('batch_id', { mode: 'text' }).unique().notNull(),
   owner_id: text('owner_id', { mode: 'text' }).notNull(),
@@ -21,14 +22,21 @@ export const files = sqliteTable('files', {
     mode: 'text',
     enum: ['user', 'admin', 'superadmin']
   }).default('user'),
-  status: text('status', {
+  upload_status: text('upload_status', {
     mode: 'text',
     enum: ['uploading', 'completed', 'failed']
   })
     .notNull()
     .default('uploading'),
+  vector_status: text('vector_status', {
+    mode: 'text',
+    enum: ['processing', 'completed', 'failed']
+  }),
   created_at: text('created_at').notNull().default(new Date().toISOString()),
-  completed_at: text('completed_at', { mode: 'text' }).default('')
+  upload_completed_at: text('upload_completed_at', { mode: 'text' }).default(
+    ''
+  ),
+  vector_completed_at: text('vector_completed_at', { mode: 'text' }).default('')
 });
 
 export const fileRelations = relations(files, ({ one }) => ({
@@ -57,6 +65,10 @@ export const selectFileSchema = z.object({
   batch_id: z.string(),
   owner_id: z.string(),
   access_level: z.enum(['user', 'admin', 'superadmin']),
-  status: z.enum(['uploading', 'completed', 'failed']),
-  owner: selectUserSchema
+  upload_status: z.enum(['uploading', 'completed', 'failed']),
+  vector_status: z.enum(['processing', 'completed', 'failed']).optional(),
+  upload_completed_at: z.string(),
+  vector_completed_at: z.string(),
+  owner: selectUserSchema.optional(),
+  created_at: z.string()
 });
