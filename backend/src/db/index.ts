1ebdb93c974cb014131cbacd2255c1548f480556
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-06-29 22:57:38 +0000

    backend: add file schema to db client

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -2,6 +2,7 @@ import { Database } from 'bun:sqlite';
 import { drizzle } from 'drizzle-orm/bun-sqlite';
 import env from '../lib/env';
 import * as directory from './schema/directory.schema';
+import * as file from './schema/file.schema';
 import * as message from './schema/message.schema';
 import * as thread from './schema/thread.schema';
 import * as user from './schema/user.schema';
@@ -9,5 +10,5 @@ import * as user from './schema/user.schema';
 const sqlite = new Database(env.DB_FILE_NAME);
 export const db = drizzle({
   client: sqlite,
-  schema: { ...directory, ...message, ...thread, ...user }
+  schema: { ...directory, ...message, ...thread, ...user, ...file }
 });
