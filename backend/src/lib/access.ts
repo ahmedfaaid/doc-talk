24c828c0d946fc8a380bdb4a6a25621753677cad
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-06-30 08:23:49 +0000

    backend: get accessible roles

--- backend/src/lib/access.ts
+++ backend/src/lib/access.ts
@@ -5,4 +5,11 @@ function hasAccess(userRole: UserRole, requiredRole: UserRole) {
   return roleHeirarchy.indexOf(userRole) >= roleHeirarchy.indexOf(requiredRole);
 }
 
+export function getAccessibleRoles(userRole: UserRole) {
+  const userLevelIndex = roleHeirarchy.indexOf(userRole);
+  if (userLevelIndex === -1) return [];
+
+  return roleHeirarchy.slice(0, userLevelIndex + 1);
+}
+
 export default hasAccess;
