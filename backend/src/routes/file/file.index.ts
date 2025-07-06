14a22c566ea53a01978453a1f768d50031cc8e56
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-07-06 10:40:06 +0000

    backend: add upload file progress route and controller to file router

--- backend/src/routes/file/file.index.ts
+++ backend/src/routes/file/file.index.ts
@@ -2,9 +2,8 @@ import { fileController } from '../../controllers';
 import { createRouter } from '../../lib/create-app';
 import * as routes from './file.route';
 
-const router = createRouter().openapi(
-  routes.uploadFile,
-  fileController.uploadFile
-);
+const router = createRouter()
+  .openapi(routes.uploadFile, fileController.uploadFile)
+  .openapi(routes.uploadProgress, fileController.uploadProgress);
 
 export default router;
