78a61c73e8c2478a16823d6917d030dddc264a62
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-07-06 10:38:43 +0000

    backend: upload file progress route

--- backend/src/routes/file/file.route.ts
+++ backend/src/routes/file/file.route.ts
@@ -5,7 +5,9 @@ import {
 } from '../../db/schema/file.schema';
 import { notFoundSchema, serverErrorSchema } from '../../lib/constants';
 import * as HttpStatusCodes from '../../lib/http-status-codes';
+import idParamsSchema from '../../lib/id-params';
 import { jsonContent } from '../../lib/json-content';
+import streamContent from '../../lib/stream-content';
 
 const tags = ['files'];
 
@@ -48,4 +50,41 @@ export const uploadFile = createRoute({
   }
 });
 
+export const uploadProgress = createRoute({
+  tags,
+  method: 'get',
+  path: '/files/upload/progress/{id}',
+  request: {
+    params: idParamsSchema
+  },
+  responses: {
+    [HttpStatusCodes.OK]: streamContent(
+      z.object({
+        data: z.string(),
+        event: z.string()
+      }),
+      'The upload progress stream'
+    ),
+    [HttpStatusCodes.BAD_REQUEST]: jsonContent(
+      z.object({
+        message: z.string(),
+        code: z.number()
+      }),
+      'Validation error for file upload progress'
+    ),
+    [HttpStatusCodes.NOT_FOUND]: jsonContent(
+      z.object({
+        message: z.string(),
+        code: z.number()
+      }),
+      'Upload not found'
+    ),
+    [HttpStatusCodes.INTERNAL_SERVER_ERROR]: jsonContent(
+      serverErrorSchema,
+      'Server error'
+    )
+  }
+});
+
 export type UploadFileRoute = typeof uploadFile;
+export type UploadProgressRoute = typeof uploadProgress;
