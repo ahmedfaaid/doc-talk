579e1d7fe866eea1eba26c970364454a1ac0f5fe
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-07-13 15:39:35 +0000

    backend: create blacklist operation

--- backend/src/db/operations/blacklist.operation.ts
+++ backend/src/db/operations/blacklist.operation.ts
@@ -1,16 +1,25 @@
+<<<<<<< HEAD
 import { eq } from 'drizzle-orm';
 import { db } from '..';
 import { Blacklist } from '../../types';
+=======
+import { db } from '..';
+>>>>>>> 455d251 (backend: create blacklist operation)
 import { blacklist } from '../schema/blacklist.schema';
 
 export const createBlacklist = async (
   token: string,
+<<<<<<< HEAD
   expires: number
+=======
+  expires: string
+>>>>>>> 455d251 (backend: create blacklist operation)
 ): Promise<void> => {
   return await db
     .insert(blacklist)
     .values({ token, expiresAt: new Date(expires).toISOString() });
 };
+<<<<<<< HEAD
 
 export const getBlacklist = async (
   token: string
@@ -25,3 +34,5 @@ export const getBlacklist = async (
 
   return searchedBlacklist as Blacklist;
 };
+=======
+>>>>>>> 455d251 (backend: create blacklist operation)
