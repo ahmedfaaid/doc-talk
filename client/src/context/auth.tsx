2f71a2693f28620ece8abb6de660263cac1903cd
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-07-17 13:41:19 +0000

    client: auth context and login function

---
+++ client/src/context/auth.tsx
@@ -0,0 +1,86 @@
+import { AuthResponse, AuthState, CookiesValues } from '@/types';
+import { invoke } from '@tauri-apps/api/core';
+import {
+  createContext,
+  ReactNode,
+  useCallback,
+  useContext,
+  useState
+} from 'react';
+import { useCookies } from 'react-cookie';
+
+export const AuthContext = createContext<AuthState | null>(null);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [user, setUser] = useState<{ id: string; email: string } | null>(null);
+  const [token, setToken] = useState<string | null>(null);
+  const [cookies, setCookie] = useCookies<'doc-talk-qid', CookiesValues>([
+    'doc-talk-qid'
+  ]);
+
+  const login = useCallback(
+    async (loginRequest: { email: string; password: string }) => {
+      setLoading(true);
+
+      try {
+        const { email, password } = loginRequest;
+        const res: AuthResponse = await invoke('login', {
+          credentials: { email, password }
+        });
+
+        if (!res.token && !res.user) {
+          setUser(null);
+          setToken(null);
+          setLoading(false);
+          return {
+            message: res.message,
+            success: false
+          };
+        }
+
+        setCookie('doc-talk-qid', {
+          token: res.token,
+          userId: res.user?.id,
+          email: res.user?.email
+        });
+        setUser(res.user!);
+        setToken(res.token!);
+        setLoading(false);
+        return {
+          token: res.token,
+          user: res.user,
+          message: 'Login successful',
+          success: true
+        };
+      } catch (error) {
+        console.log({ error });
+        setUser(null);
+        setToken(null);
+        setLoading(false);
+        return {
+          success: false,
+          message: (error as Error).message,
+          code: 500
+        };
+      }
+    },
+    []
+  );
+
+  return (
+    <AuthContext.Provider value={{ loading, user, token, login }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth hook must be used within an Auth Provider');
+  }
+
+  return context;
+};
