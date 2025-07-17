33e9c46e8b1206e38d3429deac6231a516056d0b
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-07-17 13:29:03 +0000

    client: user and auth types

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -22,3 +22,63 @@ export interface Message {
   role: string;
   content: string;
 }
+
+export type UserRole = 'user' | 'admin' | 'superadmin';
+
+export interface User {
+  id: string;
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  company: string | null;
+  isCompany: boolean;
+  role: UserRole;
+  parentId: string | null;
+  parent?: User;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface SigninPayload {
+  email: string;
+  password: string;
+}
+
+export interface RegisterPayload {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  isCompany: boolean;
+  company: string;
+  role: UserRole;
+  parentId: 'string';
+}
+
+export interface AuthResponse {
+  token?: string;
+  user?: {
+    id: string;
+    email: string;
+  };
+  message?: string;
+  code?: number;
+  success?: boolean;
+}
+
+export interface AuthState {
+  login: (data: { email: string; password: string }) => Promise<AuthResponse>;
+  loading: boolean;
+  user: {
+    id: string;
+    email: string;
+  } | null;
+  token: string | null;
+}
+
+export interface CookiesValues {
+  token: string;
+  userId: string;
+  email: string;
+}
