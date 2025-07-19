311bf7cd1c60a9b101c576685262d232cd826709
ahmedfaaid <ahmedfaaid.coding@gmail.com>
2025-07-19 13:47:00 +0000

    client: logout tauri command

--- client/src-tauri/src/auth.rs
+++ client/src-tauri/src/auth.rs
@@ -50,3 +50,22 @@ pub async fn me(token: String) -> Result<serde_json::Value, String> {
 
     Ok(body)
 }
+
+#[tauri::command]
+pub async fn logout(token: String) -> Result<serde_json::Value, String> {
+    let client = Client::new();
+
+    let res = client
+        .get("http://localhost:5155/api/auth/logout")
+        .bearer_auth(token)
+        .send()
+        .await
+        .map_err(|e| e.to_string())?;
+
+    let body = res
+        .json::<serde_json::Value>()
+        .await
+        .map_err(|e| e.to_string())?;
+
+    Ok(body)
+}
