db61e213c7aa905a3a67ba2387651a37d7618f89
ahmedfaaid <ahmedfaaid.coding@gmail.com>
2025-07-21 20:41:05 +0000

    client: only use layout component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,7 @@
-import { useContext } from 'react';
-import ChatView from './components/chat-view';
 import Layout from './components/layout';
-import NoDirectory from './components/no-directory';
-import { SelectedDirectoryContext } from './context/directory-dialog';
 
 function App() {
-  const { directory, indexed } = useContext(SelectedDirectoryContext);
-
-  return (
-    <Layout>{directory && indexed ? <ChatView /> : <NoDirectory />}</Layout>
-  );
+  return <Layout />;
 }
 
 export default App;
