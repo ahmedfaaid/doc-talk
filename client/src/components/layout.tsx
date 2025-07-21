b328a541e41f9feb4ed127c7a251f451da8ec81c
ahmedfaaid <ahmedfaaid.coding@gmail.com>
2025-07-21 20:39:13 +0000

    client: use react router outlet

--- client/src/components/layout.tsx
+++ client/src/components/layout.tsx
@@ -1,13 +1,15 @@
-import { ReactNode } from 'react';
+import { Outlet } from 'react-router';
 import AppSidebar from './app-sidebar';
 import { SidebarProvider } from './ui/sidebar';
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default function Layout() {
   return (
     <div>
       <SidebarProvider>
         <AppSidebar />
-        <main className='w-full h-screen pb-2'>{children}</main>
+        <main className='h-screen w-full pb-2'>
+          <Outlet />
+        </main>
       </SidebarProvider>
     </div>
   );
